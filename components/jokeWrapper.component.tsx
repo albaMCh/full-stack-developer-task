d500b425fe02a6f5ecb78f79b2539d43a9b7b983
albamolera <albamolerachacon@gmail.com>
2022-12-17 13:47:51 +0100

    add joke wrapper for manage jokes data and logic

---
+++ components/jokeWrapper.component.tsx
@@ -0,0 +1,50 @@
+import { IJoke } from "../shared/models/Joke";
+import Joke from "../components/joke.component";
+import { useEffect, useState, useRef } from "react";
+
+function JokeWrapper({ data }: { data: IJoke[] }) {
+  const [jokes, setJokes] = useState(data);
+  const [joke, setJoke] = useState<IJoke | undefined>(undefined);
+  const [showPunchline, setShowPunchline] = useState(false);
+
+  const [clicsNumber, setClicsNumber] = useState(0);
+
+  function loadJoke(jokes: IJoke[]) {
+    const randomNumber = Math.floor(Math.random() * jokes.length);
+    setJoke(jokes[randomNumber]);
+  }
+
+  useEffect(() => {
+    if (clicsNumber % 2 === 0) {
+      loadJoke(jokes);
+    }
+
+    const handleClickOutside = (event: { target: any }) => {
+      const newNumber = clicsNumber + 1;
+      setClicsNumber(newNumber);
+
+      if (newNumber % 2 === 0) {
+        setShowPunchline(false);
+      } else {
+        setShowPunchline(true);
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [jokes, clicsNumber]);
+
+  if (joke) {
+    return (
+      <div>
+        <p>Click anywhere for see the answer and again for next joke.</p>
+        <Joke data={joke} showPunchline={showPunchline}></Joke>
+      </div>
+    );
+  }
+
+  return <p>Content could not be loaded.</p>;
+}
+
+export default JokeWrapper;
